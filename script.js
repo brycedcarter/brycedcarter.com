f7272bacb4b8b90dce0998d74eedc8c28093997e
carter.br <brycedcarter@gmail.com>
2013-10-25 01:36:36 -0400

    simplified and improved version.

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by carter.br on 10/25/13.
+ */
+
+function mainScript()
+{
+    setTimeout(function(){$("#engineer").animate({opacity:'1'},1000)},1000)
+    setTimeout(function(){$("#designer").animate({opacity:'1'},1000)},2000)
+    setTimeout(function(){$("#creator").animate({opacity:'1'},1000)},3000)
+}
